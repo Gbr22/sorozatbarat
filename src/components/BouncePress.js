0e8372af9268a23b4500173209c995ce74bb553e
Gábor Kriskó <gabor.krisko@gmail.com>
2020-08-06 13:32:13 +0200

    BouncePress transition

--- src/components/BouncePress.js
+++ src/components/BouncePress.js
@@ -1,47 +1,58 @@
 import React from "react";
 import { TouchableHighlight, Text, Alert, StyleSheet } from "react-native";
 import { TouchableWithoutFeedback, TouchableOpacity } from "react-native-gesture-handler";
-
+import * as Animatable from 'react-native-animatable';
 
 export default class TouchableButton extends React.Component {
     constructor(props) {
         super(props);
         this.children = props.children;
-        this.state = {
-            pressed: false,
-        };
+        
     }
+    viewRef;
     
     render() {
-        var s = [styles.button];
-        if (this.state.pressed){
-            s.push(styles.buttonPressed);
+        var s = {
+            flex: 1,
+            transform: [{
+                scale:1,
+            }]
         }
+        
+        var transition = (v)=>{
+            this.viewRef.transitionTo({ scale: v });
+        }
+        
         return (
-            <TouchableWithoutFeedback
-                onPress={this.props.onPress}
-                style={s}
-                onPressOut={() => {
-                    this.setState({ pressed: false });
-                }}
-                onPressIn={() => {
-                    this.setState({ pressed: true });
-                }}
-            >
-                {this.children}
-            </TouchableWithoutFeedback>    
+            
+                <TouchableWithoutFeedback
+                    onPress={this.props.onPress}
+                    style={{
+                        flex:1,
+                    }}
+                    onPressOut={() => {
+                        transition(1);
+                    }}
+                    onPressIn={() => {
+                        transition(0.95);
+                    }}
+                >
+                    <Animatable.View
+                        style={s}
+                        transition="scale"
+                        easing="ease"
+                        ref={e=>this.viewRef=e}
+                    >
+                        {this.children}
+                    </Animatable.View>
+                    
+                </TouchableWithoutFeedback>    
+            
         );
         
     }
 }
 
 const styles = StyleSheet.create({
-    button: {
-        flex: 1
-    },
-    buttonPressed: {
-        transform: [{
-            scale: 0.97
-        }],
-    }
+    
 });
