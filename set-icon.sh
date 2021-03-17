c8da4c3c25f61e45c1a949139067b6b2843f6878
Gábor Kriskó <gabor.krisko@gmail.com>
2021-03-17 23:41:19 +0100

    icon svg -> png

--- set-icon.sh
+++ set-icon.sh
@@ -1,2 +1,3 @@
+inkscape -w 1024 -h 1024 icon/icon.svg -o icon/icon.png &&
 react-native set-icon --platform android --path icon/icon.png --background "#FFA41B" &&
 git restore android/app/src/main/AndroidManifest.xml #set-icon breaks AndroidManifest.xml, this reverses that
